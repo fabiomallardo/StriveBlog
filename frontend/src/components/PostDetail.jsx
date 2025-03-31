8b341306124277f571dcea90eed7633aa22ad71e
fabiomallardo <fabio.mallardo16@gmail.com>
2025-03-31 17:58:35 +0200

    modifica

--- frontend/src/components/PostDetail.jsx
+++ frontend/src/components/PostDetail.jsx
@@ -44,7 +44,7 @@ const PostDetail = () => {
     // Aggiungi il controllo dei dati
     const author = userNome && userCognome ? `${userNome} ${userCognome}` : "Anonimo";
     
-    const commentData = { author, text: newComment };
+    const commentData = { author : fullName, text: newComment };
     console.log("Dati che invio:", commentData);
   
     try {
