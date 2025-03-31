d036a339788d6e9ea84a2cb8566492ea45bcd7ea
fabiomallardo <fabio.mallardo16@gmail.com>
2025-03-31 12:04:10 +0200

    modifiche

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import AuthForm from "../components/AuthForm.jsx";
 import BlogPostsList from "../components/BlogPostsList.jsx";
-import sfondo from "../assets/sfondo-login.jpg";
+import sfondo from "../assets/sfondo-login.jpg"; // Importa l'immagine di sfondo
 
 const Homepage = () => {
   const [mode, setMode] = useState(null);
@@ -9,12 +9,14 @@ const Homepage = () => {
   const [userName, setUserName] = useState(localStorage.getItem("userNome") || "");
   const [userGender, setUserGender] = useState(localStorage.getItem("userGender") || "");
 
+  // Quando il componente viene caricato, aggiorniamo lo stato in base al token di autenticazione
   useEffect(() => {
     setIsAuthenticated(!!localStorage.getItem("token"));
     setUserName(localStorage.getItem("userNome") || "");
     setUserGender(localStorage.getItem("userGender") || "");
   }, []);
 
+  // Funzione di logout
   const handleLogout = () => {
     localStorage.clear();
     setIsAuthenticated(false);
@@ -24,6 +26,7 @@ const Homepage = () => {
     window.location.reload();
   };
 
+  // Funzione di login success
   const handleLoginSuccess = () => {
     setIsAuthenticated(true);
     setUserName(localStorage.getItem("userNome") || "");
@@ -32,25 +35,26 @@ const Homepage = () => {
     window.location.reload();
   };
 
+  // Funzione per togglare la visualizzazione del form
   const toggleForm = (formType) => {
     setMode((prev) => (prev === formType ? null : formType));
   };
 
   return (
     <div
-    className={`${
-      !isAuthenticated ? "vh-100 vw-100" : ""
-    }`} // Applica vh-100 solo quando non è autenticato
-    style={{
-      margin: 0,
-      padding: 0,
-      backgroundImage: !isAuthenticated ? `url(${sfondo})` : "none", // Applica lo sfondo solo quando non è autenticato
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-      overflow: "hidden",
-    }}
-  >
+      className={`${
+        !isAuthenticated ? "vh-100 vw-100" : "" // Applica vh-100 e vw-100 solo quando non è autenticato
+      }`} 
+      style={{
+        margin: 0,
+        padding: 0,
+        backgroundImage: !isAuthenticated ? `url(${sfondo})` : "none", // Applica lo sfondo solo quando non è autenticato
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        overflow: "hidden",
+      }}
+    >
       {!isAuthenticated ? (
         <div className="container h-100 d-flex flex-column justify-content-center align-items-start px-4">
           <h1 className="text-white mb-3 fs-2">Benvenuto su Strive Blog</h1>
